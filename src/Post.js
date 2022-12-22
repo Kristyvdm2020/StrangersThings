3c15748669eb484a69ef43e623fef07fb886dcfa
Kristyvdm2020 <kristyvdm2020@gmail.com>
2022-12-22 10:35:10 -0500

    moved deletePost to api folder and corrected callbacks in Post and Posts;

--- src/Post.js
+++ src/Post.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link, useParams} from 'react-router-dom';
+import { deletePost } from './api/';
 
 
 const Post = (props) => {
@@ -9,27 +10,13 @@ const Post = (props) => {
   if(!post) {
     return null;
   }
-  const deletePost = () => {
-    fetch(`https://strangers-things.herokuapp.com/api/2209-FTB-ET-WEB-AM/posts/${post._id}`, {
-      method: "DELETE",
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${token}`
-      }
-    })
-    .then(response => response.json())
-    .then(result => {
-      console.log(result);
-    })
-    .catch(console.error);
-  }
-
+  
   return (
     <div>
       <h1><Link to='/posts'>{post.title}</Link></h1> 
       <span>
         {user._id === post.author._id ? (<button className="edit-button">Edit</button>) : null}
-        { user._id === post.author._id ? (<button className="delete-button" onClick={deletePost}>Delete</button>):null}
+        { user._id === post.author._id ? (<button className="delete-button" onClick={() => deletePost(post._id, token)}>Delete</button>):null}
       </span>
       <p>Listed by: {post.author.username} ({post.location})</p>
       <p>Listing Price: {post.price}</p>
